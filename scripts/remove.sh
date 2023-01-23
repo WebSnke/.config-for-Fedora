0f0779edc1f822d134dacf371b419b4b1103234f
WebSnke <websnke@tutanota.com>
2023-01-23 17:27:48 +0100

    Add RemoveAppList

---
+++ scripts/remove.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RemoveAppList=(
+    "cheese" #GNOME Cheese
+    "gnome-boxes" #GNOME Boxes
+    "gnome-calendar" #GNOME Calendar
+    "gnome-connections" #GNOME Connections
+    "gnome-contacts" #GNOME Contacts
+    "gnome-font-viewer" #GNOME Font Viewer
+    "gnome-getting-started-docs" #GNOME Getting Started
+    "gnome-maps" #GNOME Maps
+    "gnome-system-monitor" #GNOME System Monitor
+    "gnome-tour" #GNOME Tour
+    "gnome-weather" #GNOME Weather
+    "gucharmap" #GNOME Character Map
+    "info" #Info
+    "libreoffice" #LibreOffice
+    "liveusb-creator" #Fedora Media Writer
+    "rhythmbox" #Rhythmbox
+    "simple-scan" #Document Scanner
+    "totem" #GNOME Videos
+    "totem-plugins" #GNOME Videos
+    "ubuntu-docs" #Docs
+    "yelp" #Help
+)
