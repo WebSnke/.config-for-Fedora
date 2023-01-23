838794c585290c44e1dc621ffd2ce5b83c6deb9d
WebSnke <websnke@tutanota.com>
2023-01-23 19:33:20 +0100

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,21 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+InstallAppList=(
+    "discord"
+    "freetube"
+    "spotify"
+    "steam"
+    "tutanota"
+    "visual-studio-code"
+    "zsh"
+    "zsh-autosuggestions"
+)
+
+function InstallAppFunction() {
+    for App in ${InstallAppList[@]}
+    do
+        brew install $App
+    done
+}
+
+InstallAppFunction
