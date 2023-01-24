9e461a44d86e083d5646d5c9e49dba1987929120
WebSnke <websnke@tutanota.com>
2023-01-24 19:57:04 +0100

    Remove Homebrew

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,4 @@
-sudo yum groupinstall 'Development Tools' #Add Development Tools for Homebrew
-sudo yum install procps-ng curl file git
-
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo #Install Flathub
-
-BrewInstallAppList=(
-    "zsh"
-    "zsh-autosuggestions"
-    "zsh-syntax-highlighting"
-)
 
 FedoraInstallAppList=(
     "blender"
@@ -23,11 +14,6 @@ FlatpakInstallAppList=(
     "tutanota"
     "visualstudio"
 )
-
-for BrewApp in ${BrewInstallAppList[@]}
-do
-    brew install $BrewApp
-done
 
 for FedoraApp in ${FedoraInstallAppList[@]}
 do
