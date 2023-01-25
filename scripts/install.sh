9397b609eb68f1376f1f14ef2bc6854837ff9366
WebSnke <websnke@tutanota.com>
2023-01-25 13:05:37 +0100

    Remove Comments

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo #Install Flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 FedoraInstallAppList=(
     "blender"
@@ -31,4 +31,4 @@ sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install ffmpeg
 
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" #Install Oh My Zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
