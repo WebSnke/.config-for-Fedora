291ed440115dc8b64d820c8adb8f982b02b3e70c
WebSnke <websnke@tutanota.com>
2023-01-25 13:06:31 +0100

    Add zsh-autosuggestions

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,1 +1,3 @@
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
