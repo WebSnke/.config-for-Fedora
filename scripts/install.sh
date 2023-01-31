d6e1be2c19783e53065acdf9f08345467834acd2
WebSnke <websnke@tutanota.com>
2023-01-31 18:44:41 +0100

    Add chsh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,6 +10,8 @@ do
     dnf install $FedoraApp
 done
 
+chsh
+
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install ffmpeg
