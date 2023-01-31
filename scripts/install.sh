7624bbad6ea3724aa2bfa3a442aa58e183f83725
WebSnke <websnke@tutanota.com>
2023-01-31 14:45:32 +0100

    Remove Flathub

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,31 +1,13 @@
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 FedoraInstallAppList=(
-    "blender"
     "gparted"
-    "htop"
     "neofetch"
-    "util-linux-user"
     "vim"
     "zsh"
-)
-
-FlatpakInstallAppList=(
-    "freetube"
-    "spotify"
-    "steam"
-    "tutanota"
-    "visualstudio"
 )
 
 for FedoraApp in ${FedoraInstallAppList[@]}
 do
     dnf install $FedoraApp
-done
-
-for FlatpakApp in ${FlatpakInstallAppList[@]}
-do
-    flatpak install $FlatpakApp
 done
 
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
