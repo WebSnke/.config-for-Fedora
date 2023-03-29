8d33e6c306a198d11de50d13571df81d0d7d5c99
WebSnke <websnke@tutanota.com>
2023-03-29 16:40:38 +0200

    Add Steam and remove Tutanota

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,26 +1,17 @@
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 FedoraInstallAppList=(
+    "code"
+    "ffmpeg"
     "gparted"
     "lm_sensors"
-    "neofetch"
-    "nodejs"
-    "util-linux-user"
-    "vim"
-    "zsh"
 )
 
 FlathubInstallAppList=(
     "com.discordapp.Discord"
-    "com.tutanota.Tutanota"
     "com.spotify.Client"
-    "org.chromium.Chromium"
+    "com.valvesoftware.Steam"
 )
-
-for FedoraApp in ${FedoraInstallAppList[@]}
-do
-    dnf install $FedoraApp
-done
 
 for FlathubApp in ${FlathubInstallAppList[@]}
 do
@@ -29,10 +20,11 @@ done
 
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install ffmpeg
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-dnf check-update
-sudo dnf install code
+for FedoraApp in ${FedoraInstallAppList[@]}
+do
+    dnf install $FedoraApp
+done
