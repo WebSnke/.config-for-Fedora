8b9ea0554bf7cbe148d1fee57525fc81c3621e7f
WebSnke <websnke@tutanota.com>
2023-05-13 21:11:27 +0200

    Remove non-free

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,9 +17,6 @@ do
     flatpak install flathub $FlathubApp
 done
 
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
