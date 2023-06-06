26c9a72f045846c4bbe865d42fe8a1ee0a31d323
WebSnke <websnke@tutanota.com>
2023-06-06 17:05:19 +0200

    Add ffmpeg

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,13 +15,16 @@ FlathubInstallAppList=(
 
 for FlathubApp in ${FlathubInstallAppList[@]}
 do
-    flatpak install flathub $FlathubApp -y
+    flatpak install -y flathub $FlathubApp
 done
+
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 for FedoraApp in ${FedoraInstallAppList[@]}
 do
-    dnf install $FedoraApp -y
+    dnf install -y $FedoraApp 
 done
