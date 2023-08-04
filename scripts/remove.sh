e18faa6b4ecaddfebe2adbdd7f33a5a32c679e5c
Sönke Joppien <websnke@tutanota.com>
2023-08-04 16:38:25 +0200

    Remove GNOME Calendar

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -5,17 +5,14 @@ RemoveAppList=(
     "baobab" #GNOME Disk Usage Analyzer
     "gnome-abrt" #GNOME Problem Reporting
     "gnome-boxes" #GNOME Boxes
-    "gnome-calendar" #GNOME Calendar
     "gnome-characters" #GNOME Characters
     "gnome-clocks" #GNOME Clocks
     "gnome-connections" #GNOME Connections
-    "gnome-contacts" #GNOME Contacts
     "gnome-disk-utility" #GNOME Disks
     "gnome-font-viewer" #GNOME Font Viewer
     "gnome-getting-started-docs" #GNOME Getting Started
     "gnome-maps" #GNOME Maps
     "gnome-tour" #GNOME Tour
-    "gnome-weather" #GNOME Weather
     "gucharmap" #GNOME Character Map
     "info" #Info
     "libreoffice*" #LibreOffice
